e2a99c1ebe857cf23139e24056e6c27fafc6bd18
IreneRomero <irene_romero_95@hotmail.com>
2025-02-21 11:35:24 +0100

    corregido fin captura

--- src/componets/Captura/Captura.js
+++ src/componets/Captura/Captura.js
@@ -4,9 +4,11 @@ import { createButton, printNewPointsTemplate } from '../Button/Button';
 let puntuacion = 0;
 let intervalo;
 let pausado = true;
+let juegoTerminado = false;
 
 export const inicializarcaptura = () => {
   puntuacion = 0;
+  juegoTerminado = false;
   printNewPointsTemplate(puntuacion);
   captura();
 };
@@ -48,13 +50,25 @@ export const captura = () => {
 
 const createGema = () => {
   const divPizarra = document.querySelector('#pizarra');
-  let randomHor = Math.random() * (window.innerWidth - 100);
-  let randomVer = Math.random() * (window.innerHeight - 200);
+  //Añadiendo para controlar zona
+  const rectPizarra = divPizarra.getBoundingClientRect();
+
+  const margen = 20; // Ajusta según necesites
+
+  let randomHor =
+    Math.random() * (rectPizarra.width - 100 - margen * 2) + margen;
+  let randomVer =
+    Math.random() * (rectPizarra.height - 100 - margen * 2) + margen;
+
+  // let randomHor = Math.random() * (window.innerWidth - 100);
+  // let randomVer = Math.random() * (window.innerHeight - 200);
 
   const imgGema = document.createElement('img');
   imgGema.className = 'gema';
-  imgGema.style.top = `${randomVer + 150}px`;
-  imgGema.style.left = `${randomHor}px`;
+  // imgGema.style.top = `${randomVer + 150}px`;
+  // imgGema.style.left = `${randomHor}px`;
+  imgGema.style.top = `${rectPizarra.top + randomVer}px`;
+  imgGema.style.left = `${rectPizarra.left + randomHor}px`;
   imgGema.style.transform = `rotate(${Math.random() * 360}deg)`;
   imgGema.classList.add('activa');
 
@@ -66,27 +80,49 @@ const createGema = () => {
 };
 
 const recogerGema = (e) => {
+  console.log(juegoTerminado);
+  if (juegoTerminado) return;
   puntuacion++;
   localStorage.setItem('puntoscaptura', puntuacion);
   const puntuacionHTML = document.querySelector('#points');
   puntuacionHTML.textContent = `Puntuación: ` + puntuacion;
+
+  /// No funciona
+
+  if (puntuacion >= 5) {
+    juegoTerminado = true;
+  }
+
+  if (juegoTerminado == true) {
+    mostrarVictoria(); // Mostrar mensaje de victoria
+    clearInterval(intervalo); // Detener el intervalo de las gemas
+    intervalo = null;
+  }
+
   const cofre = document.querySelector(`#cofre`);
   const posicionCofre = cofre.getBoundingClientRect();
   console.log(posicionCofre);
   e.target.style.top = `${posicionCofre.top}px`;
   e.target.style.left = `${posicionCofre.left}px`;
 
-  // let randomVer = Math.random() * 20 + 110;
-  // let randomHor = Math.random() * 25 + 200;
-  // e.target.style.top = `${window.innerHeight - randomVer}px`;
-  // e.target.style.left = `${window.innerWidth - randomHor}px`;
   setTimeout(() => {
     e.target.remove();
-  }, '1000');
+  }, '300');
 };
 
 const iniciarJuego = () => {
+  if (juegoTerminado) {
+    return;
+  }
   intervalo = setInterval(() => {
     createGema();
   }, 700);
+};
+
+const mostrarVictoria = () => {
+  const victoriaMensaje = document.createElement('div');
+  victoriaMensaje.id = 'victoriaMensaje';
+  victoriaMensaje.textContent = '¡Victoria! Has alcanzado 5 puntos';
+
+  document.querySelector('#pizarra').appendChild(victoriaMensaje);
 };
